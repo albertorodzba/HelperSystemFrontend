a3bb2ad1564e51ce5373e606d63b6c14df8e5b4d
Mario Alberto Rodriguez Bautista <alberto_rdzbautista@hotmail.com>
2023-05-30 15:57:20 -0600

    [FEAT]: applicant forms (WIP)

--- src/app/modules/applicant/pages/applicant-register/applicant-register.component.ts
+++ src/app/modules/applicant/pages/applicant-register/applicant-register.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-applicant-register',
   templateUrl: './applicant-register.component.html',
   styleUrls: ['./applicant-register.component.css']
 })
-export class ApplicantRegisterComponent {
+export class ApplicantRegisterComponent implements AfterViewInit {
+  @ViewChild('progressBar') progressBar!: ElementRef <HTMLDivElement>;
+  currentForm: number = 1;
+  totalForms: number = 4;
+  progress: number = 100/4;
+  
+  ngAfterViewInit(): void {
+    this.setProgress(this.progress);
+  }
+  
 
+  previousForm(){
+    if(this.currentForm > 1){
+      this.progress = (100/this.totalForms*this.currentForm) - (100/this.totalForms)
+      this.currentForm --;
+      this.setProgress(this.progress);
+    }
+    console.log(this.progress)
+  }
+  nextForm(){
+    if(this.currentForm < 4){
+      this.currentForm++;
+      this.progress = 100/this.totalForms*this.currentForm;
+      this.setProgress(this.progress);
+    }
+    console.log(this.currentForm);
+  }
+
+  setProgress(progress: number){
+    this.progressBar.nativeElement.style.width = progress+"%";
+    console.log(progress+"%")
+  }
 }
