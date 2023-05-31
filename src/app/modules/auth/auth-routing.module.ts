5555caab17ad6a4bbe5195c6826f3fd0e65d92b6
Mario Alberto Rodriguez Bautista <alberto_rdzbautista@hotmail.com>
2023-05-31 15:46:42 -0600

    [FEAT]: login added

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
   {
-    path: 'login',
-    component: LoginComponent
+    path: 'signin',
+    component: SigninComponent,
+  },
+  { 
+    path: 'signup', 
+    component: SignupComponent 
   },
 ];
 
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [SigninComponent, SignupComponent],
   imports: [CommonModule, AuthRoutingModule],
+  exports:[
+    
+  ]
 })
 export class AuthModule {}
